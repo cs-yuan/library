07c5c4ce5ee4b2bfc82aab9b8fb590c47d811771
csyuan <1056190672@qq.com>
2020-12-14 17:31:15 +0800

    主页显示已完毕

--- library/static/home.js
+++ library/static/home.js
@@ -1,4 +1,4 @@
-function create_list(){
+function create_list(data){
         div1 = document.createElement("div");
         div1.className = "col-md-6";
         div2 = document.createElement("div");
@@ -8,29 +8,30 @@ function create_list(){
         div3_1.className = "col p-4 d-flex flex-column position-static"; //////
         strong1 = document.createElement("strong");
         strong1.className = "d-inline-block mb-2 text-primary";
-        strong1.innerText = "World";
+        strong1.innerText = data[0];
         h1 = document.createElement('h3');
         h1.className = "mb-0";
-        h1.innerText ="Featured post";
+        h1.innerText =data[1];
         div3_1_1 = document.createElement("div");
         div3_1_1.className = "mb-1 text-muted";
-        div3_1_1.innerText = "Nov 12";
+        div3_1_1.innerText = data[2]
         p1 = document.createElement("p");
         p1.className = "card-text mb-auto";
         p1.innerText = "This is a wider card with supporting text below as a natural lead-in to additional content.";
         a1 = document.createElement("a");
-        a1.href="http://www.4399.com";
+        a1.href=data[6];
+        console.log(a1.href)
         a1.target = "_blank";
         a1.className = "stretched-link";
-        a1.innerText = "Continue reading";
+        a1.innerText = data[3]+" "+data[4];
         div3_1.appendChild(strong1);
         div3_1.appendChild(h1);
         div3_1.appendChild(div3_1_1);
         div3_1.appendChild(p1);
         div3_1.appendChild(a1);
-        div3_2 = document.createElement("div");
-        div3_2.className = "col-auto d-none d-lg-block";
-        div3_2.style.backgroundColor = "red";
+        div3_2 = document.createElement("img");
+        // div3_2.className = "";
+        div3_2.src= "../static/img_books/"+data[5];
         div3_2.style.width ="200px";
         div3_2.style.height = "250px";
 
@@ -68,14 +69,26 @@ function show_books(data){
             data:senddata,
             type:"post",
             success:function (data) {
+                var el = document.getElementById('rowb2');
+                var childs = el.childNodes;
+                for(var i = childs .length - 1; i >= 0; i--) {
+                  el.removeChild(childs[i]);
+}
                 datalist = data['data'];
                 console.log(datalist)
-                for(item in datalist){
+                for(i =0;i<datalist.length;i++){
+                    item = datalist[i]
                     num = Math.floor(Math.random()*20);
-                    book_name = "@"+num+"@.jpg"
+                    book_img = "@"+num+"@.jpg";
                     book_id = item[0]
-                    book_name = item[1]
-                    book_id
+                    book_name = item[1].substring(0,20)
+                    book_auther = item[2]
+                    book_publish = item[3]
+                    book_year = item[4]
+                    book_href = item[5]
+                    data = [book_id,book_name,book_auther,book_publish,book_year,book_img,book_href]
+                    console.log(data)
+                    create_list(data)
                 }
 
             }
